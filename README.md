14abf979bed10e5cd9f8b4d5f12567c9f379f475
Professor-yash <108882564+Professor-yash@users.noreply.github.com>
2022-07-18 19:08:55 +0530

    uploaded file
    flutter fire uploaded file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Apps From Scratch: Flutter Travel UI
+
+[YouTube: Flutter Travel UI Tutorial | Apps From Scratch](https://youtu.be/CSa6Ocyog4U)
+
+
+# travel-app
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui/screens/home_screen.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Travel UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+//  export PATH="$PATH":"$HOME/.pub-cache/bin"
+//  export PATH="$PATH":"$HOME/flutter/3.0.4/flutter/bin"
